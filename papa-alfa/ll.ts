d08177e6ff6ec6d5f92401595a54b415f0491346
atareversei (pc) <faraji.ata92@gmail.com>
2023-12-07 19:40:18 +0330

    stack

--- papa-alfa/ll.ts
+++ papa-alfa/ll.ts
@@ -47,3 +47,47 @@ class Queue<T> {
     return this.head?.value
   }
 }
+
+type SNode<T> = {
+  value: T,
+  prev?: SNode<T>
+}
+
+class Stack<T> {
+  public length: number
+  private head?: SNode<T>
+
+  constructor() {
+    this.head = undefined
+    this.length = 0
+  }
+
+  push(item: T): void {
+    const node = {value: item} as SNode<T>
+    this.length++
+    if (!this.head) {
+      this.head = node
+      return
+    }
+
+    node.prev = this.head
+    this.head = node
+  }
+
+  pop(): T | undefined {
+    this.length = Math.max(0, this.length - 1)
+    if (this.length === 0) {
+      const head = this.head
+      this.head = undefined
+      return head?.value
+    }
+
+    const head = this.head
+    this.head = head?.prev
+    return head?.value
+  }
+
+  peek(): T | undefined {
+    return this.head?.value
+  }
+}
