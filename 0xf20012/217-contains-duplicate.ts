4d993f0cfff506f716a2c578c42efac78254cc40
atareversei (pc) <faraji.ata92@gmail.com>
2023-12-09 19:37:41 +0330

    leetcode 217

---
+++ 0xf20012/217-contains-duplicate.ts
@@ -0,0 +1,8 @@
+function contains_duplicate(nums: number[]): boolean {
+  const map = new Map<number, boolean>()
+  for (let i = 0; i < nums.length; ++i) {
+    if (map.get(nums[i])) return true
+    map.set(nums[i], true)
+  }
+  return false
+}
